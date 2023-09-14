f6d7334aec8c233efc05681b6808b034679eb2fc
Javed Chauhan <chauhanjaved@gmail.com>
2023-09-14 18:13:58 +0530

    Update in style

--- src/components/DrumMachine.js
+++ src/components/DrumMachine.js
@@ -2,14 +2,14 @@ import {  useState, React, Fragment } from 'react'
 import DrumPadElements from './DrumPadElements'
 
 export default function DrumMachine() {
-  const [audioClipText, setAudioClipText] = useState("");  
+  const [audioClipText, setAudioClipText] = useState("Audio Clip");  
 
   return (
     <Fragment>
+    <div id="display">{audioClipText}</div>        
     <div id="drum-machine">        
         <DrumPadElements setAudioClipText={setAudioClipText} />        
-    </div>
-    <div id="display">{audioClipText}</div>        
+    </div>    
     </Fragment>
   )
 }
